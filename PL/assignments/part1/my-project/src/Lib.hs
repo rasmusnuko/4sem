03b600a3e5e4258583742b41f0b2d6fd063509d5
Rasmus Nuko Jørgensen <SDU\rasjo19@imada-106319.adm.c.sdu.dk>
2021-04-29 23:30:12 +0200

    Haskell

--- PL/assignments/part1/my-project/src/Lib.hs
+++ PL/assignments/part1/my-project/src/Lib.hs
@@ -136,7 +136,7 @@ getSecond (_,a) = a
 errorInState :: State -> Int
 errorInState (State (cards, piecesA, piecesB, turn))
     | errorInCards cards = 1
-    | piecesA == (sortPieces piecesA) && piecesB == (sortPieces piecesB) = 2
+    | piecesA /= (sortPieces piecesA) && piecesB /= (sortPieces piecesB) = 2
     | (errorInPieces piecesA) || (errorInPieces piecesB) = 3
     | hasDuplicates (piecesA++piecesB) = 4
     | (turn /= 0) && (turn /= 1) = 5
